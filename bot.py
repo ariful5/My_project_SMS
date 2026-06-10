f9a53319964c9a47dc9e55d2d224dfb051264e68
It's Me Napa <66830965+ariful5@users.noreply.github.com>
2026-06-11 01:40:53 +0600

    Rename workflow file from check_sms.yml to sms-Check.yml

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TG_TOKEN    = os.environ["TELEGRAM_TOKEN"]
 TG_CHAT_ID  = os.environ["TELEGRAM_CHAT_ID"]
 GH_TOKEN    = os.environ["GH_PAT_TOKEN"]
 GH_REPO     = os.environ["GH_REPO"]
-GH_WORKFLOW = "check_sms.yml"
+GH_WORKFLOW = "sms-Check.yml"
 
 RUN_DURATION = 4 * 60 * 60
 OFFSET = 0
